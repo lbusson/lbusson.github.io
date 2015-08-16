0afc3e26ec2cc04cf367ae9c973905fc1e72cf31
quazamuhaha <quazamuhaha@hotmail.com>
2015-08-16 22:14:59 +1000

    Master

--- formsubmit.php
+++ formsubmit.php
@@ -11,10 +11,10 @@
     //$action = $_REQUEST['action']; // We dont need action for this tutorial, but in a complex code you need a way to determine ajax action nature
     //$formData = json_decode($_REQUEST['formData']); // Decode JSON object into readable PHP object
 
-    $name = $formData->{'name'}; // Get username from object
-    $email = $formData->{'email'}; // Get password from object
-    $slider = $formData->{'slider'}; // Get password from object
-    $textarea = $formData->{'textarea-a'}; // Get password from object
+    $name = $_POST->{'name'}; // Get username from object
+    $email = $_POST->{'email'}; // Get password from object
+    $slider = $_POST->{'slider'}; // Get password from object
+    $textarea = $_POST->{'textarea-a'}; // Get password from object
 ?>
 
 </body>
